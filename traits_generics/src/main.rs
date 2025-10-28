793f97302ee06780d6b177c684eb7bd2d5ebc8ff
TypeTew <tewsk132@gmail.com>
2025-10-28 21:49:54 +0700

    update

--- traits_generics/src/main.rs
+++ traits_generics/src/main.rs
@@ -1,6 +1,23 @@
 // traits
 //  สามารถเขียนโค้ดที่ทำงานกับ type ต่างๆ ที่ implement trait เดียวกัน
 //  แชร์พฤติกรรมระหว่าง types ต่างๆ
+//
+//
+//Inventory System
+
+//Crabby needs a flexible inventory system that can hold ANY
+//item (maps, gold, potions) and can display the details of those
+//items.
+//Your challenge is to:
+
+//Create a generic Inventory struct that can store any kind of
+//item: strings, integers, etc.
+//Create DisplayItem trait and Implement the DisplayItem trait
+//for the Inventory, so Crabby can display his treasures.
+// Try holding both strings (like "Diamonds") and integers (like
+// 100 coins).
+//
+
 struct Inventory<T> {
     item: T,
 }
