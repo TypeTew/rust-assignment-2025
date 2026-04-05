d3f62b115e4360cd09bbe4290a175322ccd7e3d0
TypeTew <tewsk132@gmail.com>
2026-04-05 11:16:15 +0700

    iterators-and-closures final

--- iterators-and-closures/src/main.rs
+++ iterators-and-closures/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let treasures : Vec<i32> = vec![1, 2, 3, 4, 5];
+
+    let doubled_treasures: Vec<i32> = treasures.iter().map(|x| x * 2).collect();    
+
+    println!("Treasures: {:?}", treasures);
+    println!("Doubled Treasures: {:?}", doubled_treasures);
 }
